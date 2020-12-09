f95d326808fa948d86dbb17062d1d2d74e0cd68f
joysaur <joycejwang1@gmail.com>
2020-12-08 20:35:08 -0500

    updates

--- day_5.js
+++ day_5.js
@@ -1,20 +1,5 @@
 function returnRow(row) {
   const totalRows = 128;
-  // divide by 2
-  // if bottom half 0 to num
-  // if uppder half num + 1 to last half
-  // 64 - 127 
-
-  let finalRow = 0;
-
-  row.forEach(letter => {
-    const midpoint = totalRows / 2; 
-    if(letter === 'F') {
-
-    } else if (letter === 'B') {
-
-    }
-  })
 }
 
 
@@ -26,11 +11,6 @@ function seatId(row, column) {
   return row * 8 + column;
 }
 
-function splitRowAndCol() {
-
-}
-
-// splitRowAndCol("FBFBBFFRLR")
 
 returnRow("FBFBBFF") // 44
 
